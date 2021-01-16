bd9dd9bc246993aca2e264069af7209d38e348d2
7assan-5alloof <hassankhalloof20@gmail.com>
2021-01-16 22:06:25 +0400

    Finish project

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 int main()
 {
     cout << "Press Ctrl-C to stop" << endl;
-
+    while(1)
+    {
+        int salary;
+        cout << "Please enter your monthly salary: ";
+        cin >> salary;
+        int tax;
+        if (salary > 9999) tax = salary * .4;
+        else if (salary > 7999) tax = salary * .3;
+        else if (salary > 4999) tax = salary * .2;
+        else
+        {
+            cout << "Salary not applicable" << endl;
+            continue;
+        }
+        cout << tax << endl;
+    }
     return 0;
 }
